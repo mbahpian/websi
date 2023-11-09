fff4bb5bea6cba239487d878af68a3de071185f4
mbahpian <61449674+mbahpian@users.noreply.github.com>
2023-11-10 02:31:43 +0700

    Update form.js

--- form.js
+++ form.js
@@ -11,7 +11,7 @@ var input_blanter = document.getElementById('wa_email');
 /* Whatsapp Settings */
 var walink = 'https://web.whatsapp.com/send',
     phone = '6283831393134',
-    walink2 = 'Halo saya ingin membuat website',
+    walink2 = 'Halo kak, saya ingin membuat website',
     text_yes = 'Terkirim.',
     text_no = 'Isi semua Formulir lalu klik Send.';
 
@@ -24,6 +24,7 @@ if("" != input_blanter.value){
 
  /* Call Input Form */
 var input_select1 = $("#wa_select :selected").text(),
+    input_select2 = $("#wa_select1 :selected").text(),
     input_name1 = $("#wa_name").val(),
     input_email1 = $("#wa_email").val(),
     input_number1 = $("#wa_number").val(),
@@ -31,13 +32,11 @@ var input_select1 = $("#wa_select :selected").text(),
     input_textarea1 = $("#wa_textarea").val();
 
 /* Final Whatsapp URL */
-var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
-    '*Name* : ' + input_name1 + '%0A' +
-    '*Email Address* : ' + input_email1 + '%0A' +
-    '*Select Option* : ' + input_select1 + '%0A' +
-    '*Input Number* : ' + input_number1 + '%0A' +
-    '*URL/Link* : ' + input_url1 + '%0A' +
-    '*Description* : ' + input_textarea1 + '%0A';
+var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + " " +  "untuk" + " " + input_select2 + " " +  "dengan" + " " + input_select1 + '%0A%0A' +
+    '*Nama* : ' + input_name1 + '%0A' +
+    '*Email* : ' + input_email1 + '%0A' +
+    '*Pilihan Paket* : ' + input_select1 + '%0A' +
+    '*No Telp* : ' + input_number1 + '%0A';
 
 /* Whatsapp Window Open */
 window.open(blanter_whatsapp,'_blank');
